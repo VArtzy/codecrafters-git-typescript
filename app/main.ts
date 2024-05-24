3b619fc641962d5afa5e4a2131e39ce4dc271535
VArtz <farrelnikoson@gmail.com>
2024-05-24 16:58:23 +0700

    make hash-object

--- app/main.ts
+++ app/main.ts
@@ -1,12 +1,14 @@
 import * as fs from 'fs';
 import zlib from 'zlib';
+import crypto from 'crypto';
 
 const args = process.argv.slice(2);
 const command = args[0];
 
 enum Commands {
     Init = "init",
-    Catfile = "cat-file"
+    CatFile = "cat-file",
+    HashObject = "hash-object"
 }
 
 switch (command) {
@@ -17,7 +19,7 @@ switch (command) {
         fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
         console.log("Initialized git directory");
         break;
-    case Commands.Catfile:
+    case Commands.CatFile:
         const blobDir = args[2].substring(0, 2);
         const blobFile = args[2].substring(2);
         const blob = fs.readFileSync(`.git/objects/${blobDir}/${blobFile}`);
@@ -26,6 +28,20 @@ switch (command) {
         const blobContent = decompressedBuffer.subarray(nullByteIndex + 1).toString();
         process.stdout.write(blobContent);
         break;
+    case Commands.HashObject:
+        const flag = args[1];
+        const path = args[2];
+        const data = fs.readFileSync(path);
+        const header = `blob ${data.length}\0`;
+        const sha = crypto.createHash("sha1").update(header + data).digest("hex");
+        if (flag === "-w") {
+            const dir = sha.substring(0, 2);
+            const file = sha.substring(2);
+            const compressedData = zlib.deflateSync(file);
+            fs.mkdirSync(`.git/objects/${dir}`, { recursive: true });
+            fs.writeFileSync(`.git/object/${dir}/${file}`, compressedData);
+        }
+        process.stdout.write(sha);
     default:
         throw new Error(`Unknown command ${command}`);
 }
